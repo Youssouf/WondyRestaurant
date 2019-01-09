380c45652954f4c0137ad5c8f088f09c32a11081
Youssouf <yssouare@gmail.com>
2019-01-09 18:09:18 +0100

    Add fullName Property

--- WondyRestaurant/Models/ApplicationUser.cs
+++ WondyRestaurant/Models/ApplicationUser.cs
@@ -11,5 +11,7 @@ namespace WondyRestaurant.Models
     {
         public string FirstName { get; set; }
         public string LastName { get; set; }
+        public string FullName { get; set; }
+
     }
 }
